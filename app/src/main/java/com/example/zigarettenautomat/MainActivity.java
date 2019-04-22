59c4ae32185ae1ec2d9f526df46e8e451dd52ad7
Kevin Jakob <jakob-kevin@gmx.de>
2019-04-22 22:15:50 +0200

    test1

--- app/src/main/java/com/example/zigarettenautomat/MainActivity.java
+++ app/src/main/java/com/example/zigarettenautomat/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.zigarettenautomat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnOpenMaps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOpenMaps = (Button) findViewById(R.id.btnOpenMaps);
+        btnOpenMaps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), Maps.class));
+            }
+        });
     }
 }
